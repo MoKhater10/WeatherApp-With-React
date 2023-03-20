4cc45dd546d1e1cc9bdb9c44e2eb3ae588123247
MoKhater10 <mostafa.khater17@gmail.com>
2023-03-20 02:14:42 +0200

    First  Uplod

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+const API_KEY = "016fb5a72bd47d82cb717b3c5e91f9ec";
+// http://api.openweathermap.org/data/2.5/weather?q=cairo,egypt&appid=e36ed364400282e43250b6c4c0274d44
+class App extends Component{
+
+  state = {
+    tempreature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error:''
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api.json()
+    if (city && country) {
+      this.setState({
+        tempreature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    } else {
+      this.setState({
+            tempreature: '',
+            city: '',
+            country: '',
+            humidity: '',
+            description: '',
+            error:'Please Enter Data'
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-container">
+          <Form getWeather={this.getWeather} />
+          <Weather
+              tempreature = {this.state.tempreature}
+              city = {this.state.city}
+              country = {this.state.country}
+              humidity = {this.state.humidity}
+              description = {this.state.description}
+              error = {this.state.error}
+            />
+          </div>
+      </div>
+    )
+  }
+}
+export default App;
